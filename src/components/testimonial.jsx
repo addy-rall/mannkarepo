967d6745a36966e92875b94725302bbc0b5c48f5
Adya <amishra9336@gmail.com>
2025-09-10 12:15:28 +0530

    Create testimonial.jsx

---
+++ src/components/testimonial.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../App.css"; // import the CSS file
+import author1 from "../assets/profile1.jpg";
+import author2 from "../assets/profile2.jpg";
+import author3 from "../assets/profile3.webp";    
+
+const TestimonialsSection = () => {
+  return (
+    <section className="testimonials-section">
+      <h2>What People Say About Us</h2>
+      <div className="testimonials-container">
+        <div className="testimonial-card">
+          <p>
+            "Great service and support! They really helped us scale our
+            business."
+          </p>       
+          <div className="testimonial-author">
+            <img src={author1} alt="Author 1" />
+            <span>Rohan Srivastava</span>
+          </div>
+          </div>
+          <div className="testimonial-card">
+          <p>
+            "One of the best services I had experienced in my life ."
+          </p>
+          <div className="testimonial-author">
+            <img src={author2} alt="Author 2" />
+            <span>Smita Tripathi</span>
+          </div>
+          </div>
+          <div className="testimonial-card">
+          <p>
+          "Loved the service! Very Helpful. Indeed a life changer "
+          </p>
+          <div className="testimonial-author">
+            <img src={author3} alt="Author 3" />
+            <span>Shruti Sharma</span>
+          </div>
+          </div>
+          </div>
+    </section>
+  );
+};
+
+export default TestimonialsSection;
